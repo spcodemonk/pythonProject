28dcee9fa5f87944cee0cfd572c79440426e821f
spcodemonk <sharadpawar01@gmail.com>
2024-02-11 20:31:37 +0530

    initial commit

---
+++ pack1/Fqr.py
@@ -0,0 +1,5 @@
+
+
+
+def myFunction():
+    return 123
---
+++ sp.py
@@ -0,0 +1,9 @@
+import os
+from pack1 import Fqr
+def main1():
+    print("-------------How are you sharad--------------")
+
+    print(Fqr.myFunction())
+
+if __name__ == "__main__":
+    main1()
